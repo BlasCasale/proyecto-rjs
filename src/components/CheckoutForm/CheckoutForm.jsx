1554939278db8287df95b3e63feac0ceaf17e025
BlasCasale <117787465+BlasCasale@users.noreply.github.com>
2023-05-12 10:13:23 -0300

    Update CheckoutForm.jsx

--- src/components/CheckoutForm/CheckoutForm.jsx
+++ src/components/CheckoutForm/CheckoutForm.jsx
@@ -49,7 +49,7 @@ const CheckoutForm = () => {
         const date = new Date()
 
         const day = date.getDate()
-        const month = date.getMonth()
+        const month = date.getMonth() + 1
         const year = date.getFullYear()
 
         const order = {
